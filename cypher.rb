31bcf6ff78d78755e55d11a2317e0f158e81a03d
Salah <salahaldahdouh@gmail.com>
2026-02-10 19:42:06 +0300

    make final change and its working

--- cypher.rb
+++ cypher.rb
@@ -1,10 +1,28 @@
 def caesar_cipher(text, shift)
-  text.chars.map do |char|
-    if char.match?(/[a-zA-Z]/)
-      base = char.downcase == char ? 'a'.ord : 'A'.ord
-      ((char.ord - base + shift) % 26 + base).chr
+  #pseudocode for caesar cypher
+  #for every letter in text
+  #if the letter is a-z or A-Z, take the number of the letter and deduct the shift value
+  #wrap if the result is out of range
+  #add the new letter to the result
+  # code starts here
+  letters = text.split('')
+  result = []
+  letters.each do |i|
+    if i.match?(/[a-z]/) 
+      res = (i.ord + shift - 'a'.ord) % 26 + 'a'.ord
+      result.push(res.chr)
+    
+    elsif i.match?(/[A-Z]/) 
+      res = (i.ord + shift - 'A'.ord) % 26 + 'A'.ord
+      result.push(res.chr)
+    
     else
-      char
+      result.push(i)
     end
-  end.join
+  
 end
+puts result.join('')
+
+
+end
+caesar_cipher('zebra1', 1)
